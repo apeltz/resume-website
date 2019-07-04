0d7e51d09741a9a6136f3e58123bf84b9afeb9a1
Aaron Peltz <aaron.peltz@gmail.com>
2019-07-04 01:49:14 -0700

    style cleanups

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -12,6 +12,8 @@ class App extends Component {
 		e.preventDefault();
 		let activeView = e.currentTarget.dataset.section;
 		let content = e.currentTarget.getElementsByTagName('div')[1];
+		console.log('CONTENT: ', content);
+		// content.style.maxHeight = content.style.maxHeight ? null : "100%";
 		content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + 'px';
 		this.setState({ activeView });
 	}
@@ -30,11 +32,13 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className={style.wrapper} >
-				<SideNav views={this.views} handleNavSelection={this.handleNavSelection} />
-				<div className={style.largeContent} >
-					<h2 className={style.sectionTitle}>{this.state.activeView}</h2>
-					{this.views[this.state.activeView]}
+			<div className={style.outer_wrapper} >
+				<div className={style.inner_wrapper}>
+					<SideNav views={this.views} handleNavSelection={this.handleNavSelection} />
+					<div className={style.largeContent} >
+						<h2 className={style.sectionTitle}>{this.state.activeView}</h2>
+						{this.views[this.state.activeView]}
+					</div>
 				</div>
 			</div>
 		);
--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -10,28 +10,27 @@ function Contact() {
 		backgroundPosition: 'center'
 	};
 	return (
-		<div className={style.contact} style={divStyle}>
-			<table>
-				<thead />
-				<tbody>
-					<tr>
-						<td >Name:</td>
-						<td className={style.value}>Aaron Peltz</td>
-					</tr>
-					<tr>
-						<td>Email:</td>
-						<td className={style.value}><a target="_blank" rel="noopener noreferrer" href="mailto:aaron.peltz@gmail.com">aaron.peltz@gmail.com</a></td>
-					</tr>
-					<tr>
-						<td>LinkedIn:</td>
-						<td className={style.value}><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/apeltz">linkedin.com/in/apeltz</a></td>
-					</tr>
-					<tr>
-						<td>Github:</td>
-						<td className={style.value}><a target="_blank" rel="noopener noreferrer" href="https://www.github.com/apeltz">github.com/apeltz</a></td>
-					</tr>
-				</tbody>
-			</table>
+		<div className={style.contact}>
+			<div></div>
+			<div className={style.inner_wrapper} style={divStyle}>
+				<table>
+					<thead />
+					<tbody>
+						<tr>
+							<td >Name:</td>
+							<td className={style.value}>Aaron Peltz</td>
+						</tr>
+						<tr>
+							<td>LinkedIn:</td>
+							<td className={style.value}><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/apeltz">linkedin.com/in/apeltz</a></td>
+						</tr>
+						<tr>
+							<td>Github:</td>
+							<td className={style.value}><a target="_blank" rel="noopener noreferrer" href="https://www.github.com/apeltz">github.com/apeltz</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
