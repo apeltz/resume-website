aaa3ce123c2ecfd4eebf45db0e2142ce33bd0fbd
Aaron Peltz <aaron.peltz@gmail.com>
2021-07-13 19:02:12 -0700

    Basic nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,66 @@
 import React from 'react';
-import './App.css';
 import { Resume } from './pages/resume'
 import avatar from './assets/ap_avatar.jpg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { AppBar, Avatar, Box, Icon, Grid, Container, ListItem, ListItemIcon, ListItemText, makeStyles, Typography } from '@material-ui/core'
+import DescriptionIcon from '@material-ui/icons/Description';
+
+const useStyles = makeStyles(() => ({
+  header: {
+     backgroundColor: "royalblue",
+     padding: '1rem',
+  },
+  navLink: {
+    color: 'white',
+    minWidth: '20px',
+    textDecoration: 'none',
+  }
+}));
 
 function App() {
+  const { header, navLink } = useStyles();
   return (
-    <div className="App">
-      <img src={avatar} alt="avatar" />
-      <Resume />
-    </div>
+    
+      <Router>
+        <Container maxWidth="lg">
+
+        <Grid container xs={12} >
+    <Grid item xs={12}>
+
+        <AppBar position="static" className={header}>
+
+
+          <Link to="/resume" className={navLink}   >                 
+          <Box display="flex" alignItems='center'>
+                      <DescriptionIcon fontSize="large" />
+                      <Typography style={{ fontSize: 25}}>Resume</Typography>
+
+          </Box>
+                      </Link>
+
+        </AppBar>
+
+
+        <Switch>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/">
+            <Resume />
+          </Route>
+        </Switch>
+    </Grid>
+
+        </Grid>
+        </Container>
+    </Router>
   );
 }
 
 export default App;
+
